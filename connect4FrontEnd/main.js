d667ed1684fb2014115aaf661aa709b1d2027493
Tommy Pham <tommyp101@live.com>
2022-07-06 16:21:35 -0500

    sendMoves function registers click

--- connect4FrontEnd/main.js
+++ connect4FrontEnd/main.js
@@ -5,3 +5,19 @@ window.addEventListener("DOMContentLoaded", () => {
   const board = document.querySelector(".board");
   createBoard(board);
 });
+
+function sendMoves(board, websocket) {
+  // When clicking a column, send a "play" event for a move in that column.
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+    // Ignore clicks outside a column.
+    if (column === undefined) {
+      return;
+    }
+    const event = {
+      type: "play",
+      column: parseInt(column, 10),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
