db53c5e763244ba092ca14c6d31d47c7675dbf48
Tommy Pham <tommyp101@live.com>
2022-07-08 17:33:35 -0500

    Server to Browser websocket

--- connect4FrontEnd/main.js
+++ connect4FrontEnd/main.js
@@ -5,8 +5,11 @@ window.addEventListener("DOMContentLoaded", () => {
   const board = document.querySelector(".board");
   createBoard(board);
   const websocket = new WebSocket("ws://localhost:8001/");
+  recieveMoves(board, websocket);
   sendMoves(board, websocket);
 });
+
+
 
 function sendMoves(board, websocket) {
   // When clicking a column, send a "play" event for a move in that column.
@@ -21,5 +24,32 @@ function sendMoves(board, websocket) {
       column: parseInt(column, 10),
     };
     websocket.send(JSON.stringify(event));
+  });
+}
+
+
+// Recieve the event data from server, server used data from browser -> server to alter.
+function showMessage(message)
+{
+  window.setTimeout(() => window.alert(message), 50);
+}
+
+function recieveMoves(board, websocket) {
+  websocket.addEventListener("message", ({data}) => { // Receive websocket message by listening to message events.
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "play":
+        playMove(board, event.player, event.column, event.row);
+        break;
+      case "win":
+        showMessage(`Player ${event.player} wins!`);
+        websocket.close(1000);
+        break;
+      case "error":
+        showMessage(event.message);
+        break;
+      default:
+        throw new Error(`Unsupported event type: ${event.type}.`);
+    }
   });
 }
